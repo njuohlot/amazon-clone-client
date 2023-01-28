d805b6f24e2e7c5eedd03a51d00da409d9d5d710
njuohlot <njuohlottimngum@gmail.com>
2023-01-28 09:06:24 +0100

    fff

---
+++ src/Admin/components/Analytic/Analytic.js
@@ -0,0 +1,27 @@
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import Topbar from '../topbar/Topbar'
+
+
+export default function Analytic() {
+ 
+  return (
+    <>
+    <Topbar/>
+
+    <div className='container'>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <div className="homeWidgets">
+        <WidgetLg/>
+      </div>
+    </div>
+
+    
+    </>
+    
+   
+  );
+}
---
+++ src/Admin/components/Transactions/Transactions.js
@@ -0,0 +1,21 @@
+
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import Topbar from '../topbar/Topbar'
+
+export default function Transactions() {
+  return (
+    <>
+    <Topbar/>
+
+    <div className='container'>
+      <div className="homeWidgets">
+        <WidgetLg/>
+      </div>
+    </div>
+
+    
+    </>
+    
+   
+  );
+}
